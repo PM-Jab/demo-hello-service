da686e4e7c95cf983c8d435222a283e6a7bd35d3
Possawee Jab <possawee.m@arise.tech>
2026-05-03 22:36:15 +0700

    feat: add mock apis

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+type helloResponse struct {
+	Message string `json:"message"`
+}
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/hello", func(c *gin.Context) {
+		c.JSON(200, helloResponse{
+			Message: "Hello, World!",
+		})
+	})
+
+	r.POST("/hello/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		c.JSON(200, helloResponse{
+			Message: "Hello, " + name + "!",
+		})
+	})
+
+	r.POST("/hi/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		c.JSON(200, helloResponse{
+			Message: "Hi, " + name + "!",
+		})
+	})
+
+	r.Run(":8080")
+}
